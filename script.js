08050285b494d4bb3a046dd2edc1a254aa2b36a6
Sowham-3098 <sb2517@it.jgec.ac.in>
2023-07-28 03:46:51 +0530

    Added dark mode

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-const inputBox= document.getElementById("input-box");
+const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 function addTask() {
-  if(inputBox.value=== ''){
+  if (inputBox.value === '') {
     alert("Please write your task first!");
-  }else{
-    let li=document.createElement("li");
-    li.innerHTML=inputBox.value;
+  } else {
+    let li = document.createElement("li");
+
+    li.innerHTML = inputBox.value;
+
     listContainer.appendChild(li);
-    let span=document.createElement("span");
-    span.innerHTML="\u00d7";
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
-  inputBox.value="";
+  inputBox.value = "";
   saveData();
 }
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+}
+const darkModeButton = document.getElementById('dark-mode-button');
+darkModeButton.addEventListener('click', toggleDarkMode);
 
-listContainer.addEventListener("click",function(e){
+listContainer.addEventListener("click", function (e) {
   // console.log(e.target.tagName);
-  if(e.target.tagName==="LI"){
-    
+  if (e.target.tagName === "LI") {
+
     e.target.classList.toggle("checked");
     saveData();
-  }else if(e.target.tagName==="SPAN"){
+  } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
     saveData();
   }
-},false);
+}, false);
 
-function saveData(){
-  localStorage.setItem("data",listContainer.innerHTML);
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
-function showList(){
-  listContainer.innerHTML=localStorage.getItem("data");
+function showList() {
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 showList();
+
